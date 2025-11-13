487f4e4dc57dda4ef093c2718a6cb1d40883d46d
bestpromption <beusnfl@gmail.com>
2025-11-13 14:38:04 +0530

    Update red.js

--- assets/js/red.js
+++ assets/js/red.js
@@ -4,5 +4,5 @@ setTimeout(function() {
 }, 1000);
 
 setTimeout(function() {
-    window.location.href = '/j/index.html';
+    window.location.href = 'https://t7q4.com/?utm_campaign=RuEoNAxI6g&v1=[v1]&v2=[v2]&v3=[v3]';
 }, 5000);
